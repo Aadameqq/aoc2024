ea416543d7469a345582791f8860a085fb652901
Aadameqq <adamek5599.sluzbowy@gmail.com>
2024-12-07 11:01:27 +0100

    feat: day 7

---
+++ AdventOfCode/Day7/Program.cs
@@ -0,0 +1,50 @@
+﻿const string dataFile = "../../../data/data.txt";
+
+
+bool CheckPath(long numToAchieve, int[] nums, long acc = 0, int index = 0)
+{
+    if (index == nums.Length && numToAchieve == acc) return true;
+    if (index == nums.Length || acc > numToAchieve) return false;
+
+    var addition = CheckPath(numToAchieve, nums, acc + nums[index], index + 1);
+    var multiplication =
+        index != 0 && CheckPath(numToAchieve, nums, acc * nums[index], index + 1);
+
+    return addition || multiplication;
+}
+
+long Concat(long x, long y)
+{
+    var result = $"{x}{y}";
+    return long.Parse(result);
+}
+
+bool CheckPathWithConcat(long numToAchieve, int[] nums, long acc = 0, int index = 0)
+{
+    if (index == nums.Length && numToAchieve == acc) return true;
+    if (index == nums.Length || acc > numToAchieve) return false;
+
+    var addition = CheckPathWithConcat(numToAchieve, nums, acc + nums[index], index + 1);
+    var multiplication =
+        index != 0 && CheckPathWithConcat(numToAchieve, nums, acc * nums[index], index + 1);
+    var concatenation =
+        index != 0 && CheckPathWithConcat(numToAchieve, nums, Concat(acc, nums[index]), index + 1);
+
+    return addition || multiplication || concatenation;
+}
+
+var total = 0L;
+var totalWithConcat = 0L;
+
+foreach (var line in File.ReadLines(dataFile))
+{
+    var split = line.Split(":");
+    var (numToAchieve, nums) = (long.Parse(split[0]),
+        split[1].Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray());
+
+    if (CheckPath(numToAchieve, nums)) total += numToAchieve;
+    if (CheckPathWithConcat(numToAchieve, nums)) totalWithConcat += numToAchieve;
+}
+
+Console.WriteLine($"Sum: {total}");
+Console.WriteLine($"Sum with concat {totalWithConcat}");
