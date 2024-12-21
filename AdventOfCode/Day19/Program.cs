337724107d4dd7041d1cc3b69d3ff6aeef2faa6d
Aadameqq <adamek5599.sluzbowy@gmail.com>
2024-12-21 18:57:54 +0100

    feat: day 19 part 1

---
+++ AdventOfCode/Day19/Program.cs
@@ -0,0 +1,32 @@
+﻿List<string> availableTowels = [];
+
+const string dataFile = "../../../data/data.txt";
+var input = File.ReadAllLines(dataFile);
+
+
+foreach (var towel in input[0].Split(", ").ToArray())
+    availableTowels.Add(towel);
+
+var patterns = input[2..];
+
+bool IsPossible(string pattern)
+{
+    if (pattern.Length == 0) return true;
+
+    foreach (var towel in availableTowels)
+        if (pattern.EndsWith(towel))
+        {
+            var trimmedPattern = pattern[..^towel.Length];
+            if (IsPossible(trimmedPattern)) return true;
+        }
+
+    return false;
+}
+
+var total = 0;
+
+foreach (var pattern in patterns)
+    if (IsPossible(pattern))
+        total++;
+
+Console.WriteLine(total);
