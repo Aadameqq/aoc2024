2620caec2a85839ad506318fdbf4077daa7dfe8c
Aadameqq <adamek5599.sluzbowy@gmail.com>
2024-12-21 19:59:30 +0100

    feat: day 19 part 2

--- AdventOfCode/Day19/Program.cs
+++ AdventOfCode/Day19/Program.cs
@@ -3,41 +3,42 @@
 const string dataFile = "../../../data/data.txt";
 var input = File.ReadAllLines(dataFile);
 
-
 foreach (var towel in input[0].Split(", ").ToArray())
     availableTowels.Add(towel);
 
 var patterns = input[2..];
 
-var patternsCache = new Dictionary<string, int>();
+var patternsCache = new Dictionary<string, long>();
 
-int CountPossible(string pattern, string current)
+long CountPossible(string pattern)
 {
+    if (patternsCache.TryGetValue(pattern, out var count)) return count;
     if (pattern.Length == 0) return 1;
 
-
-    if (patternsCache.TryGetValue(pattern, out var count)) return count;
-
-    var possible = 0;
+    var possible = 0L;
 
     foreach (var towel in availableTowels)
         if (pattern.EndsWith(towel))
         {
             var trimmedPattern = pattern[..^towel.Length];
-            possible += CountPossible(trimmedPattern, pattern[^towel.Length..] + current);
+            possible += CountPossible(trimmedPattern);
         }
 
-    patternsCache.TryAdd(current, possible);
+    patternsCache.TryAdd(pattern, possible);
+
     return possible;
 }
 
-var total = 0;
+var totalPossible = 0;
+var totalWays = 0L;
 
 foreach (var pattern in patterns)
 {
-    var possible = CountPossible(pattern, "");
-    patternsCache.Add(pattern, possible);
-    if (possible > 0) total++;
+    var possible = CountPossible(pattern);
+    totalWays += possible;
+    if (possible > 0)
+        totalPossible++;
 }
 
-Console.WriteLine(total);
+Console.WriteLine(totalPossible);
+Console.WriteLine(totalWays);
